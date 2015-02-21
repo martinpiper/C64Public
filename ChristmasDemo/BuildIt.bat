6b5b64e846f50884c1c981d79327037dc3eae5c0
martinpiper <martin.piper@gmail.com>
2015-02-21 16:55:46 +0800

    Adding resurrected sources for the Christmas Demo
    Originally released 1991
    Includes tweaks to assemble with ACME instead of 6501+

---
+++ ChristmasDemo/BuildIt.bat
@@ -0,0 +1,12 @@
+@echo off
+del ChristmasDemo.prg
+
+..\acme.exe -v3 --msvc ChristmasDemo.a
+if not exist ChristmasDemo.prg goto error
+..\bin\LZMPi.exe -c64b ChristmasDemo.prg ChristmasDemo.prg $400 >t.txt
+if not exist ChristmasDemo.prg goto error
+goto end
+:error
+echo ChristmasDemo.prg not created!
+exit -1
+:end
