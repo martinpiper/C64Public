1dcfbc3edbc22de58f5391dabf64cf32508cf844
Martin Piper <martin.piper@gmail.com>
2023-11-12 17:29:53 +0800

    Tape streamed samples with IRQ

--- IRQTape/TapeStreamSamples.bat
+++ IRQTape/TapeStreamSamples.bat
@@ -1,4 +1,8 @@
-..\acme.exe --lib  ../ -v4 --msvc TapeStreamSamples.a
+rem ..\acme.exe --lib  ../ -v4 --msvc TapeStreamSamples.a
+..\acme.exe --lib  ../ -v4 --msvc TapeStreamSamplesIRQ.a
 ..\bin\LZMPi.exe -c64bmu TapeStreamSamples.prg TapeStreamSamples.prg $400
-..\bin\TapeTool.exe wn "vice.tap" q c:\temp\t.wav 22050 $4e 965536 48 16 985248 c
+
+rem This must match kStartCycleOffset and kPerVolumeCycles used in the code
+..\bin\TapeTool.exe wn "vice.tap" q c:\temp\t.wav 22050 $4e 965536 56 16 985248 c
+
 C:\Downloads\WinVICE-3.1-x86-r34062\WinVICE-3.1-x86-r34062\x64sc.exe -remotemonitor -dstapewobble 0 -dsspeedtuning 0 -1 vice.tap TapeStreamSamples.prg
