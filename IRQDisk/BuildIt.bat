9576d4e5d0ad96038bcfc89095122908fa3bb841
Martin Piper <martin.piper@gmail.com>
2024-11-08 14:27:38 +0800

    Smallest boot

--- IRQDisk/BuildIt.bat
+++ IRQDisk/BuildIt.bat
@@ -15,10 +15,15 @@ del TestIRQDisk3.prg
 ..\acme.exe -v4 --msvc TestIRQDisk3.a
 if not exist TestIRQDisk3.prg goto error
 
+del SmallestBoot.prg
+..\acme.exe -v4 --pdb SmallestBoot.pdb --msvc -f cbm -o SmallestBoot.prg SmallestBoot.a
+if not exist SmallestBoot.prg goto error
+
+
 goto end
 
 :error
 echo TestIRQDisk.prg not created!
 exit -1
 :end
-..\ExternalTools\C1541\c1541.exe -interleave 15 -attach test.d64 -format test,67 -write TestIRQDisk.prg testirqdisk -write screen1.prg screen1 -write screen2.prg screen2 -write screen3.prg screen3 -write TESTSAVE.PRG testsave
+..\ExternalTools\C1541\c1541.exe -interleave 15 -attach test.d64 -format test,67 -write SmallestBoot.prg boot -write TestIRQDisk.prg testirqdisk -write screen1.prg screen1 -write screen2.prg screen2 -write screen3.prg screen3 -write TESTSAVE.PRG testsave
